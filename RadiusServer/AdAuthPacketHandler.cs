4f977656d3a3ed630a84d4ee8fab9bf926a6b8a7
Ketano Tinca <ketano.tinca@gmail.com>
2021-03-15 11:42:24 +0200

    Add project files.

---
+++ RadiusServer/AdAuthPacketHandler.cs
@@ -0,0 +1,73 @@
+﻿using Flexinets.Radius.Core;
+using log4net;
+using System;
+using System.Configuration;
+using System.DirectoryServices.AccountManagement;
+
+namespace RadiusServer
+{
+    public class AdAuthPacketHandler : IPacketHandler
+    {
+        private readonly ILog _log = LogManager.GetLogger(typeof(AdAuthPacketHandler));
+        public IRadiusPacket HandlePacket(IRadiusPacket packet)
+        {
+            var config = ConfigurationManager.AppSettings;
+            var domain = config.Get("Domain");
+            var groupName = config.Get("AdGroupName");
+            _log.Info($"Recived request with packet code {packet.Code}");
+            if(packet.Code == PacketCode.AccessRequest)
+            {
+                var userName = packet.GetAttribute<String>("User-Name");
+                var userPassword = packet.GetAttribute<String>("User-Password");
+                var userLogin = ValidateCredentials(domain, userName, userPassword, userName, userName);
+                var userInGroup = IsUserInAdGroup(domain, userName, groupName, userName, userPassword);
+                _log.Info($"User Login result ={userLogin} UserInGroup result = {userInGroup} for user {userName}");
+                if(userInGroup && userLogin)
+                {
+                    var response = packet.CreateResponsePacket(PacketCode.AccessAccept);
+                    response.AddAttribute("Acct-Interim-Interval", 60);
+                    return response;
+                }
+
+                return packet.CreateResponsePacket(PacketCode.AccessReject);
+            }
+
+            _log.Info($"Cant handle request code {packet.Code}");
+
+            throw new InvalidOperationException($"Can't handle other requests besides AccessRequests with code {PacketCode.AccessRequest}");
+        }
+
+        private bool ValidateCredentials(string domain, string username, string password, string servAccUser, string servAccPass)
+        {
+            var result = false;
+            using (var context = new PrincipalContext(ContextType.Domain, domain, servAccUser, servAccPass))
+            {
+                result = context.ValidateCredentials(username, password);
+            }
+
+            return result;
+        }
+
+        private bool IsUserInAdGroup(string domain, string username, string adGroupName, string servAccUser, string servAccPass)
+        {
+            bool result = false;
+            using (var context = new PrincipalContext(ContextType.Domain, domain, servAccUser, servAccPass))
+            {
+                var user = UserPrincipal.FindByIdentity(context, username);
+                if (user != null)
+                {
+                    var group = GroupPrincipal.FindByIdentity(context, adGroupName);
+                    if (group != null && user.IsMemberOf(group))
+                        result = true;
+                    group.Dispose();
+                }
+                user.Dispose();
+            }
+            return result;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ RadiusServer/Service1.cs
@@ -0,0 +1,61 @@
+﻿using Flexinets.Net;
+using Flexinets.Radius.Core;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Net;
+using System.Configuration;
+using log4net;
+
+namespace RadiusServer
+{
+    public partial class RadiusServerService : ServiceBase
+    {
+        private Flexinets.Radius.RadiusServer _authenticationServer;
+        private readonly ILog _log = LogManager.GetLogger(typeof(RadiusServerService));
+        public RadiusServerService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                _log.Info("Starting service");
+                var path = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory) + "/Content/radius.dictionary";
+                var dictionary = new RadiusDictionary(path, NullLogger<RadiusDictionary>.Instance);
+                var radiusPacketParser = new RadiusPacketParser(NullLogger<RadiusPacketParser>.Instance,dictionary);
+                var packetHandler = new AdAuthPacketHandler();
+                var repository = new Flexinets.Radius.PacketHandlerRepository();
+                var udpClientFactory = new UdpClientFactory();
+                repository.AddPacketHandler(IPAddress.Any, packetHandler, "secret");
+
+                var port = int.Parse(ConfigurationManager.AppSettings.Get("Port"));
+
+                _authenticationServer = new Flexinets.Radius.RadiusServer(
+                    udpClientFactory,
+                    new IPEndPoint(IPAddress.Any, port),
+                    radiusPacketParser,
+                    Flexinets.Radius.RadiusServerType.Authentication,
+                    repository,
+                    NullLogger<Flexinets.Radius.RadiusServer>.Instance
+                    );
+
+                _authenticationServer.Start();
+
+            }catch(Exception ex)
+            {
+                _log.Fatal("Failed to start service", ex);
+                throw;
+            }
+        }
+
+        protected override void OnStop()
+        {
+            _authenticationServer?.Stop();
+            _authenticationServer?.Dispose();
+        }
+    }
+}
